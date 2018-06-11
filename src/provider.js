e80d6ea2b3735457a04d18b10bfd0abbf0a82ac6
SAPer <gsaper@gmail.com>
2018-06-12 01:13:41 +0300

    add provider

---
+++ src/provider.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch');
+const htmlParser = require('fast-html-parser');
+const store = require('./store');
+
+
+module.exports = { init };
+
+async function init() {
+
+}
