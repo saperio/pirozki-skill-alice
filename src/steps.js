4fd473c9d9002bd89c8b541b8b9497124caa97d0
SAPer <gsaper@gmail.com>
2018-06-19 00:10:14 +0300

    update steps

--- src/steps.js
+++ src/steps.js
@@ -1,6 +1,15 @@
-const { DB_USERS, STEP_NEW_USER, STEP_NAME, STEP_SECOND } = require('./constants');
 const provider = require('./provider');
 const store = require('./store');
+const {
+	DB_USERS,
+	STEP_NEW_USER,
+	STEP_NAME,
+	STEP_SECOND,
+	STEP_COMEBACK,
+	PAYLOAD_MORE,
+	PAYLOAD_TWO_IN_ROW,
+	PAYLOAD_THREE_IN_ROW
+} = require('./constants');
 
 function updateUser(user, data) {
 	store.update(DB_USERS, user.id, data);
@@ -27,6 +36,16 @@ function getRandomPieIdx(user) {
 	return pieIdx;
 }
 
+function checkPayload(payload, checkValue) {
+	if (!payload) {
+		return false;
+	}
+
+	return payload.value === checkValue;
+}
+
+//user.inRow = 2;
+
 module.exports = {
 	[STEP_NEW_USER]: ({ user }) => {
 		user.step = STEP_NAME;
@@ -34,14 +53,25 @@ module.exports = {
 	},
 
 	[STEP_NAME]: async ({ command, user }) => {
+		user.step = STEP_SECOND;
+
+		const button = {
+			title: 'Еще еще!!!',
+			hide: true,
+			payload: {
+				value: PAYLOAD_MORE
+			}
+		};
+
 		// reject
 		if (command.indexOf('не ') !== -1 || command.indexOf('нет ') !== -1 || command === 'нет') {
-			user.step = STEP_SECOND;
-
 			const pieIdx = getRandomPieIdx(user);
 			const { text } = await provider.best(pieIdx);
 
-			return `Ладно, вот тебе первый пирожок!\n${text}`;
+			return {
+				text: `Ладно, вот тебе первый пирожок!\n${text}\n\nЕще?`,
+				buttons: [button]
+			};
 		}
 
 		// cleanup name
@@ -52,21 +82,65 @@ module.exports = {
 			.trim()
 		;
 
-		user.step = STEP_SECOND;
 		user.name = name;
 
 		const pies = await provider.search(name);
 		if (pies.length) {
-			return pies[0].text;
+			return {
+				text: `${pies[0].text}\n\nЕще?`,
+				buttons: [button]
+			};
 		}
 
 		const pieIdx = getRandomPieIdx(user);
 		const { text } = await provider.best(pieIdx);
 
-		return text;
+		return {
+			text: `${text}\n\nЕще?`,
+			buttons: [button]
+		};
 	},
 
-	[STEP_SECOND]: async ({ command, user }) => {
+	[STEP_SECOND]: async ({ payload, command, user }) => {
+		// reject
+		if (
+			command.indexOf('хватит') !== -1,
+			command.indexOf('стоп') !== -1,
+			command.indexOf('достаточно') !== -1
+		) {
+			return {
+				text: 'До встречи, приходи еще!',
+				end_session: true
+			}
+		}
 
+		if (checkPayload(payload, PAYLOAD_MORE)) {
+		}
+
+		const pieIdx = getRandomPieIdx(user);
+		const { text } = await provider.best(pieIdx);
+
+		return {
+			text: `${text}\n\nА еще я могу читать по два и по три пирожка за раз, просто скажи «Давай по два»`,
+			buttons: [
+				{
+					title: 'Давай по два',
+					hide: true,
+					payload: {
+						value: PAYLOAD_TWO_IN_ROW
+					}
+				},
+				{
+					title: 'Давай по три',
+					hide: true,
+					payload: {
+						value: PAYLOAD_THREE_IN_ROW
+					}
+				}
+			]
+		};
+	},
+
+	[STEP_COMEBACK]: async ({ command, user }) => {
 	}
 };
