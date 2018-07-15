c5c4a3ec0023c3090e586e8fa30a207960ab6ad8
SAPer <gsaper@gmail.com>
2018-07-15 15:03:21 +0300

    cleanup store

--- src/store.js
+++ src/store.js
@@ -1,21 +1,10 @@
-const fs = require('fs').promises;
 
-module.exports = { init, set, get, update };
+module.exports = { init, set, get };
 
 
 let memory = {};
-let dirty = false;
-const dbFilename = './store.db';
 
 async function init() {
-	/*try {
-		const raw = await fs.readFile(dbFilename);
-		memory = JSON.parse(raw);
-	} catch(e) {
-		dirty = true;
-	}
-
-	await check();*/
 }
 
 function set(db, id, val) {
@@ -24,7 +13,6 @@ function set(db, id, val) {
 	}
 
 	memory[db][id] = val;
-	dirty = true;
 }
 
 function get(db, id) {
@@ -37,30 +25,4 @@ function get(db, id) {
 	}
 
 	return memory[db][id];
-}
-
-function update(db, id, val) {
-	if (!memory[db]) {
-		memory[db] = {};
-	}
-
-	if (!memory[db][id]) {
-		memory[db][id] = {};
-	}
-
-	set(db, id, { ...memory[db][id], ...val});
-}
-
-// internal api
-async function save() {
-	await fs.writeFile(dbFilename, JSON.stringify(memory));
-}
-
-async function check() {
-	if (dirty) {
-		await save();
-		dirty = false;
-	}
-
-	setTimeout(check, 1000);
 }
