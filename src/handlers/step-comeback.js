1eafa9a5b4e981bd38d23840383075f8072cf1c5
SAPer <gsaper@gmail.com>
2018-07-23 11:26:24 +0300

    fix texts

--- src/handlers/step-comeback.js
+++ src/handlers/step-comeback.js
@@ -1,12 +1,20 @@
 const { nextStep } = require('../utils');
-const { STEP_MAIN } = require('../constants');
+const { STEP_MAIN, PAYLOAD_MORE } = require('../constants');
 const { best } = require('../provider-utils');
+
 
 module.exports = async function stepComeback({ user }) {
 	const pies = await best(user);
 
 	nextStep(user, STEP_MAIN);
 	return {
-		text: `С возвращением!\n\n${pies}`
+		text: `С возвращением!\n\n${pies}`,
+		buttons: [{
+			title: 'Еще еще!!!',
+			hide: true,
+			payload: {
+				value: PAYLOAD_MORE
+			}
+		}]
 	};
 }
